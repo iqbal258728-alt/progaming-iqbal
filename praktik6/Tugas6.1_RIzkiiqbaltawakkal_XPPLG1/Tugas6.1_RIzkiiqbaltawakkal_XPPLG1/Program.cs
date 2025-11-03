221c4361bb27a330063a7e81ba5b9a3952d194e8
iqbal258728-alt <iqbal258728@student.smkn1kandeman.sch.id>
2025-11-03 07:46:11 +0700

    Add files via upload

---
+++ praktik6/Tugas6.1_RIzkiiqbaltawakkal_XPPLG1/Tugas6.1_RIzkiiqbaltawakkal_XPPLG1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tugas6._1_RIzkiiqbaltawakkal_XPPLG1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                string[] nama = new string[3];
+                char[] jk = new char[3];
+                string[] kelas = new string[3];
+
+                Console.Write("Masukkan nama siswa ke-1: "); nama[0] = Console.ReadLine() ?? string.Empty;
+                Console.Write("Masukkan jenis kelamin (L/P): "); jk[0] = char.Parse(Console.ReadLine() ?? "0");
+                Console.Write("Masukkan kelas: "); kelas[0] = Console.ReadLine() ?? string.Empty;
+
+                Console.Write("Masukkan nama siswa ke-2: "); nama[1] = Console.ReadLine() ?? string.Empty;
+                Console.Write("Masukkan jenis kelamin (L/P): "); jk[1] = char.Parse(Console.ReadLine() ?? "0");
+                Console.Write("Masukkan kelas: "); kelas[1] = Console.ReadLine() ?? string.Empty;
+
+                Console.Write("Masukkan nama siswa ke-3: "); nama[2] = Console.ReadLine() ?? string.Empty;
+                Console.Write("Masukkan jenis kelamin (L/P): "); jk[2] = char.Parse(Console.ReadLine() ?? "0");
+                Console.Write("Masukkan kelas: "); kelas[2] = Console.ReadLine() ?? string.Empty;
+
+                Console.WriteLine("\n=== Data Siswa ===");
+                Console.WriteLine($"Nama: {nama[0]} | Jenis Kelamin: {jk[0]} | Kelas: {kelas[0]}");
+                Console.WriteLine($"Nama: {nama[1]} | Jenis Kelamin: {jk[1]} | Kelas: {kelas[1]}");
+                Console.WriteLine($"Nama: {nama[2]} | Jenis Kelamin: {jk[2]} | Kelas: {kelas[2]}");
+
+            }
+        }
+    }
+    
+}
---
+++ praktik6/praktik6.1_Rizkiiqbaltawakkal_XPPLG1/praktik6.1_Rizkiiqbaltawakkal_XPPLG1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktik6._1_Rizkiiqbaltawakkal_XPPLG1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                string[] siswa = new string[5];
+                siswa[0] = "Budi";
+                siswa[1] = "Andi";
+                siswa[2] = "Citra";
+                siswa[3] = "Dewi";
+                siswa[4] = "Eka";
+
+                Console.WriteLine("Daftar Nama Siswa: ");
+                Console.WriteLine("Siswa ke-1: " + siswa[0] + "\nSiswa ke-2: " + siswa[1] + "\nSiswa ke-3: " + siswa[2] + "\nSiswa ke-4: " + siswa[3] + "\nSiswa ke-5: " + siswa[4]);
+
+            }
+        }
+    }
+}
